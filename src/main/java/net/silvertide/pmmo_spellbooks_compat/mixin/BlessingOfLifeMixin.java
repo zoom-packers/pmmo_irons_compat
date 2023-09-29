31a94ecb852fdbc80ce010d97ba34baa91075a67
Silvertide7 <natephillips801@gmail.com>
2023-09-28 23:18:51 -0600

    Added BA blessing of Life mixin with ModifyReceiver shwing.

--- src/main/java/net/silvertide/pmmo_spellbooks_compat/mixin/BlessingOfLifeMixin.java
+++ src/main/java/net/silvertide/pmmo_spellbooks_compat/mixin/BlessingOfLifeMixin.java
@@ -1,21 +1,25 @@
 package net.silvertide.pmmo_spellbooks_compat.mixin;
-import harmonised.pmmo.api.APIUtils;
+import com.llamalad7.mixinextras.injector.ModifyReceiver;
 import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
 import io.redspace.ironsspellbooks.spells.holy.BlessingOfLifeSpell;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlessingOfLifeSpell.class)
 public abstract class BlessingOfLifeMixin extends AbstractSpell {
-    @Inject(method = "onCast", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;heal(F)V", shift = At.Shift.AFTER), remap = false)
-    public void injectAfterHealing(Level world, int spellLevel, LivingEntity entity, MagicData playerMagicData, CallbackInfo ci) {
-        float xpAmount = 20*super.getSpellPower(spellLevel, entity) * .25f;
-        APIUtils.addXp("wisdom", (ServerPlayer) entity, (long) xpAmount);
+    @ModifyReceiver(method = "onCast", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;heal(F)V"), remap = true)
+    public LivingEntity addXpBeforeHeal(LivingEntity targetEntity, float healAmount, Level world, int spellLevel, LivingEntity entity, MagicData playerMagicData) {
+        entity.sendSystemMessage(Component.literal("You just healed " + targetEntity.getName() + " for " + healAmount));
+        return targetEntity;
     }
+
+
+//    public void injectAfterHealing(Level world, int spellLevel, LivingEntity entity, MagicData playerMagicData, CallbackInfo ci) {
+//        float xpAmount = 20*super.getSpellPower(spellLevel, entity) * .25f;
+//        APIUtils.addXp("wisdom", (ServerPlayer) entity, (long) xpAmount);
+//    }
 }
